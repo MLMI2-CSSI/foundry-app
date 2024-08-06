346218842a2aa4d281b4c5ad422f555e59748b9e
KJ Schmidt <kj.schmidt913@gmail.com>
2024-08-06 13:00:07 -0400

    Update docs link

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@ export default {
       { title: 'Home', icon: 'mdi-home', to: '/', target: '' },
       { title: 'Datasets', icon: 'mdi-magnify', to: '#/datasets', target: '' },
       { title: 'About', icon: 'mdi-chat-question', to: '#/about', target: '' },
-      { title: 'Documentation', icon: 'mdi-file-document-multiple-outline', to: 'https://ai-materials-and-chemistry.gitbook.io/foundry/v/docs/', target: 'blank' },
+      { title: 'Documentation', icon: 'mdi-file-document-multiple-outline', to: 'https://ai-materials-and-chemistry.gitbook.io/foundry-1.0', target: 'blank' },
       { title: 'Examples', icon: 'mdi-rocket-launch', to: `https://github.com/MLMI2-CSSI/foundry/tree/main/examples`, target: 'blank' }
     ]
   }),
